3d55402a64e8b681cb6337719969f12178670017
Bryan Davis <bd808@wikimedia.org>
2016-03-25 18:04:21 +0000

    Derived tables definitions
    Bug: T116065

---
+++ create-action-tables.sql
@@ -0,0 +1,58 @@
+-- Create tables for Action API stats
+--
+-- Usage:
+--     hive -f create-action-tables.sql --database wmf
+
+CREATE TABLE IF NOT EXISTS action_ua_hourly (
+  userAgent STRING COMMENT 'Raw user-agent',
+  wiki      STRING COMMENT 'Target wiki (e.g. enwiki)',
+  ipClass   STRING COMMENT 'IP based origin, can be wikimedia, wikimedia_labs or internet',
+  viewCount BIGINT COMMENT 'Number of requests'
+)
+COMMENT 'Hourly summary of Action API requests bucketed by user-agent and wiki'
+PARTITIONED BY (
+  year      INT COMMENT 'Unpadded year of request',
+  month     INT COMMENT 'Unpadded month of request',
+  day       INT COMMENT 'Unpadded day of request',
+  hour      INT COMMENT 'Unpadded hour of request'
+)
+STORED AS PARQUET;
+
+
+CREATE EXTERNAL TABLE IF NOT EXISTS action_action_hourly (
+  action    STRING COMMENT 'Action parameter value',
+  wiki      STRING COMMENT 'Target wiki (e.g. enwiki)',
+  ipClass   STRING COMMENT 'IP based origin, can be wikimedia, wikimedia_labs or internet',
+  viewCount BIGINT COMMENT 'Number of requests'
+)
+COMMENT 'Hourly summary of Action API requests bucketed by action and wiki'
+PARTITIONED BY (
+  year      INT COMMENT 'Unpadded year of request',
+  month     INT COMMENT 'Unpadded month of request',
+  day       INT COMMENT 'Unpadded day of request',
+  hour      INT COMMENT 'Unpadded hour of request'
+)
+STORED AS PARQUET;
+
+
+CREATE EXTERNAL TABLE IF NOT EXISTS action_param_hourly (
+  action    STRING COMMENT 'Action parameter value',
+  param     STRING COMMENT 'Parameter name, can be prop, list, meta, generator, etc',
+  value     STRING COMMENT 'Parameter value',
+  wiki      STRING COMMENT 'Target wiki (e.g. enwiki)',
+  ipClass   STRING COMMENT 'IP based origin, can be wikimedia, wikimedia_labs or internet',
+  viewCount BIGINT COMMENT 'Number of requests'
+)
+COMMENT 'Hourly summary of Action API requests bucketed by action, parameter, value and wiki'
+PARTITIONED BY (
+  year      INT COMMENT 'Unpadded year of request',
+  month     INT COMMENT 'Unpadded month of request',
+  day       INT COMMENT 'Unpadded day of request',
+  hour      INT COMMENT 'Unpadded hour of request'
+)
+STORED AS PARQUET;
+
+-- NOTE: there are many params we would not want to count distinct values of
+-- at all (eg maxlag, smaxage, maxage, requestid, origin, centralauthtoken,
+-- titles, pageids). It will be easier to whitelist in the ETL process
+-- than to try and selectively blacklist.
